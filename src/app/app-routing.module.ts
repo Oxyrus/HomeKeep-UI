e838df3a4d2642a7779632b9cd5bebcf98e41de7
Oxyrus <im.oxyrus@gmail.com>
2021-12-21 08:38:43 -0500

    Use hash on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'inventories',
-    loadChildren: () => import('./feature/inventory/inventory.module').then(m => m.InventoryModule)
-  }
+    loadChildren: () =>
+      import('./feature/inventory/inventory.module').then(
+        (m) => m.InventoryModule
+      ),
+  },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
